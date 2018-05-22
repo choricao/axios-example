8131f297b0a01ec81cdb918a2d83a434f40c19ff
Zheng Cao <chori84@gmail.com>
2018-05-22 15:31:00 -0700

    Implement createPet

--- index.js
+++ index.js
@@ -27,6 +27,39 @@ const loadPets = () => {
     console.log('This is after .get');
 }
 
+const createPet = (event) => {
+  // Note that createPet is a handler for a `submit`
+  // event, which means we need to call `preventDefault`
+  // to avoid a page reload
+  event.preventDefault();
+  console.log('We submitted a form!');
+
+  let petData = {};
+  petData['name'] = $('input[name="name"]').val();
+  petData['age'] = $('input[name="age"]').val();
+  petData['owner'] = $('input[name="owner"]').val();
+
+  console.log(petData);
+
+  reportStatus('Sending pet data...');
+
+  axios.post(URL, petData)
+    .then((response) => {
+      console.log(response);
+      reportStatus(`Successfully added a pet with the name ${response.data.name} and ID ${response.data.id}`);
+    })
+    .catch((error) => {
+      console.log(error.response);
+      reportStatus(`Encountered an error: ${error.message}`);
+    });
+
+  // TODO: implement clearForm();
+};
+
 $(document).ready(() => {
   $('#load').click(loadPets);
+  // $('#load').on('click', loadPets);
+
+  $('#pet-form').submit(createPet);
+  // $('#pet-form').on('submit', createPet);
 })
