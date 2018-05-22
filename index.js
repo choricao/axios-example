2a2a5a1b90fbb602b56ca63180c8e52004c1e736
Zheng Cao <chori84@gmail.com>
2018-05-22 10:09:24 -0700

    Implement load pet lists

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-/* Code goes here */
+const URL = 'https://petdibs.herokuapp.com/pets';
 
+const loadPets = () => {
+  const petList = $('#pet-list');
+  petList.empty();
+
+  // promise: then & catch
+  axios.get(URL)
+    .then((response) => {
+      console.log('inside the .then');
+      response.data.forEach((pet) => {
+        petList.append(`<li>${pet.name}</li>`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+
+    console.log('This is after .get');
+}
+
+$(document).ready(() => {
+  $('#load').click(loadPets);
+})
